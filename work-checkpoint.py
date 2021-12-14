a3579ae9017f8eed6c738155cd550814d3b1148b
tibales1 <tibales@hotmail.com>
2021-12-13 19:29:21 -0600

    adding python workspace

---
+++ work-checkpoint.py
@@ -0,0 +1,71 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "89d1eac6",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "title = \"Tom Bales\"\n",
+    "title = \"thing\"\n",
+    "years = 23\n",
+    "hourly_wage = 65.40\n",
+    "expert_status = True\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "id": "006896dc",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "thing\n",
+      "23\n",
+      "65.4\n",
+      "True\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(title)\n",
+    "print(years)\n",
+    "print(hourly_wage)\n",
+    "print(expert_status)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "483323ee",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.8"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
